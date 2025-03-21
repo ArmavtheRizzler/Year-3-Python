20a8dc7f32e586a30628804c44cf0cd9dfb67692
ArmavtheRizzler <7310@trinity.croydon.sch.uk>
2025-03-21 10:03:22 +0000

    Add files via upload

---
+++ Input explained.py
@@ -0,0 +1,17 @@
+#Python has may built in functions
+#Here is a list: https://docs.python.org/3/library/functions.html
+
+#Here is a variable
+name = "bob"
+
+#however, not everyone is called Bob
+#Here is how to allow a user to input their name
+name = str(input("enter your name"))
+#str = string
+print ("Nice to meet you", name)
+
+
+age = int(input("enter your name"))
+print("You are", age)
+
+ 
