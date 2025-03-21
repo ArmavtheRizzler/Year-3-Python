afead753c2e7dcc1813fb692cdbba5a196a30f2a
ArmavtheRizzler <7310@trinity.croydon.sch.uk>
2025-03-21 10:02:15 +0000

    Add files via upload

---
+++ Data Types Explained.py
@@ -0,0 +1,8 @@
+#Data type
+#C.R.I.B.S
+#Here are variables that hold each type of data type
+character= "4" or "!" or "N" etc.
+realFloat= 22.24 or 78.9 etc
+integer= 555 or 12 or 93 etc
+boolean= True or False
+string= "abcdefghijklmnopqrstuvwxyz"
